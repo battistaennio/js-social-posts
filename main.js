b6c7f9e59813a920767cbec4f252ecdfa6bb6369
battistaennio <ennio.battista99@gmail.com>
2024-06-20 15:12:09 +0200

    feat: create forEach cycle and print div.post in DOM

--- main.js
+++ main.js
@@ -56,3 +56,47 @@ const posts = [
     }
 ];
 
+// Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
+// Se clicchiamo sul tasto “Mi Piace” cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+
+//seleziono il container
+const container = document.getElementById("container");
+
+//ciclo forEach per andare a stampare in pagina un div.post per ogni obj dell'array posts
+posts.forEach((obj) => {
+    //creo la struttura del div.post
+    let postStructure = `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${obj.author.image}" alt="${obj.author.name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${obj.author.name}</div>
+                        <div class="post-meta__time">${obj.created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${obj.content}</div>
+            <div class="post__image">
+                <img src="${obj.media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="${obj.id}">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${obj.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+    `
+    //inserisco i div.post nel container
+    container.innerHTML += postStructure;
+});
